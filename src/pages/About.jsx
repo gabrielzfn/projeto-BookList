96a6c7e09c270ea0bae2d2dcebda6d22cbae33e7
Gabriel Zefino <gabriel.zefino@outlook.com>
2025-05-28 12:29:56 -0300

    Começo das páginas

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div className="about">
+        <h1>Sobre o projeto</h1>
+        <p>Sistema simples para gerenciamento de livros lidos, desenvolvido com REACT.</p>
+        <p>Aqui você pode cadastrar seus livros lidos com informações sobre os mesmos.</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div className="home">
+        <h1>Bem vindo ao Gerenciador de Livros!</h1>
+        <p>Gerencie sua coleção de livros lidos de forma fácil e intuitiva.</p>
+        </div>
+    );
+}
+
+export default Home;
